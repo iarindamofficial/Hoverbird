c9a335caa6f78c439bfb8df7afc30728730717cf
arindam <iarindamdutta@gmail.com>
2021-02-04 03:23:15 +0530

    Hover Bird

---
+++ app/src/main/java/com/arindamv1/hoverbird/Bird.java
@@ -0,0 +1,61 @@
+package com.arindamv1.hoverbird;
+
+public class Bird {
+
+    private int birdX, birdY, currentFrame, velocity;
+    public static int maxFrame;
+
+    public Bird(){
+       birdX = AppConstants.SCREEN_WIDTH/2 - AppConstants.getBitmapBank().getBirdWidth()/2;
+       birdY = AppConstants.SCREEN_HEIGHT/2 - AppConstants.getBitmapBank().getBirdHeight()/2;
+       currentFrame = 0;
+       maxFrame = 3;
+       velocity = 0;
+    }
+
+    // Getter method for velocity
+    public int getVelocity(){
+        return velocity;
+    }
+
+    // Setter method for velocity
+    public void setVelocity(int velocity){
+        this.velocity = velocity;
+    }
+
+
+    // Getter method for current frame
+    public int getCurrentFrame(){
+        return currentFrame;
+    }
+
+    // Setter method for current frame
+
+
+    public void setCurrentFrame(int currentFrame) {
+        this.currentFrame = currentFrame;
+    }
+
+    // Getter method for getting X-coordinate of the Bird
+    public int getX(){
+        return birdX;
+    }
+
+
+    // Getter method for getting Y-coordinate of the Bird
+    public int getY(){
+        return  birdY;
+    }
+
+    // Setter method for setting X-coordinate
+    public void setX(int birdX){
+        this.birdX = birdX;
+    }
+
+    // Setter method for setting Y-coordinate
+    public void setY(int birdY){
+        this.birdY = birdY;
+    }
+
+
+}
